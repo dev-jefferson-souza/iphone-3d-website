d76d673d05350b8c5d7d3096a5a77175b8043c90
Jefferson Souza <jefferson.souza.correa@gmail.com>
2023-03-31 12:25:45 -0300

    feat(WebgiViewer): implementando modelo 3D

---
+++ src/components/WebgiViewer.jsx
@@ -0,0 +1,69 @@
+import { useCallback, useEffect, useRef } from "react";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import {
+  AssetManagerPlugin,
+  BloomPlugin,
+  GammaCorrectionPlugin,
+  GBufferPlugin,
+  ProgressivePlugin,
+  SSAOPlugin,
+  SSRPlugin,
+  TonemapPlugin,
+  ViewerApp,
+} from "webgi";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export function WebgiViewer() {
+  const canvasRef = useRef(null);
+
+  const setupViewer = useCallback(async () => {
+    const viewer = new ViewerApp({
+      canvas: canvasRef.current,
+    });
+
+    const manager = await viewer.addPlugin(AssetManagerPlugin);
+
+    const camera = viewer.scene.activeCamera;
+    const position = camera.position;
+    const target = camera.target;
+
+    await viewer.addPlugin(GBufferPlugin);
+    await viewer.addPlugin(new ProgressivePlugin(32));
+    await viewer.addPlugin(new TonemapPlugin(true));
+    await viewer.addPlugin(GammaCorrectionPlugin);
+    await viewer.addPlugin(SSRPlugin);
+    await viewer.addPlugin(SSAOPlugin);
+    await viewer.addPlugin(BloomPlugin);
+
+    viewer.renderer.refreshPipeline();
+
+    await manager.addFromPath("scene-black.glb");
+
+    viewer.getPlugin(TonemapPlugin).config.clipBackground = true;
+
+    viewer.scene.activeCamera.setCameraOptions({ controlsEnabled: flase });
+
+    window.scrollTo(0, 0);
+
+    let needsUpdate = true;
+
+    viewer.addEventListener("preFrame", () => {
+      if (needsUpdate) {
+        camera.positionTargetUpdated(true);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    setupViewer();
+  }, []);
+
+  return (
+    <div id="webgi-canvas-container">
+      <canvas id="webgi-canvas" ref={canvasRef} />
+    </div>
+  );
+}
