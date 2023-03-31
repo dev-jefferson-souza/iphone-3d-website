d5eb492cc0e3e98131a26c7f2ce151e5f4fca793
Jefferson Souza <jefferson.souza.correa@gmail.com>
2023-03-31 15:32:57 -0300

    feat(WebgiViewer): otimizando animações para mobile

--- src/components/WebgiViewer.jsx
+++ src/components/WebgiViewer.jsx
@@ -14,6 +14,7 @@ import {
   BloomPlugin,
   GammaCorrectionPlugin,
   GBufferPlugin,
+  mobileAndTabletCheck,
   ProgressivePlugin,
   SSAOPlugin,
   SSRPlugin,
@@ -32,6 +33,7 @@ export const WebgiViewer = forwardRef((props, ref) => {
   const [positionRef, setPositionRef] = useState(null);
   const canvasContainerRef = useRef(null);
   const [previewMode, setPreviewMode] = useState(false);
+  const [isMobile, setIsMobile] = useState(null);
 
   useImperativeHandle(ref, () => ({
     triggerPreview() {
@@ -56,11 +58,14 @@ export const WebgiViewer = forwardRef((props, ref) => {
     },
   }));
 
-  const memoizedScrollAnimation = useCallback((position, target, onUpdate) => {
-    if (position && target && onUpdate) {
-      scrollAnimation(position, target, onUpdate);
-    }
-  }, []);
+  const memoizedScrollAnimation = useCallback(
+    (position, target, isMobile, onUpdate) => {
+      if (position && target && onUpdate) {
+        scrollAnimation(position, target, isMobile, onUpdate);
+      }
+    },
+    []
+  );
 
   const setupViewer = useCallback(async () => {
     const viewer = new ViewerApp({
@@ -68,6 +73,8 @@ export const WebgiViewer = forwardRef((props, ref) => {
     });
 
     setViewerRef(viewer);
+    const isMobileOrTablet = mobileAndTabletCheck();
+    setIsMobile(isMobileOrTablet);
 
     const manager = await viewer.addPlugin(AssetManagerPlugin);
 
@@ -95,6 +102,12 @@ export const WebgiViewer = forwardRef((props, ref) => {
 
     viewer.scene.activeCamera.setCameraOptions({ controlsEnabled: false });
 
+    if (isMobileOrTablet) {
+      position.set(-16.7, 1.17, 11.7);
+      target.set(0, 1.37, 0);
+      props.contentRef.current.className = "mobile-or-tablet";
+    }
+
     window.scrollTo(0, 0);
 
     let needsUpdate = true;
@@ -111,7 +124,7 @@ export const WebgiViewer = forwardRef((props, ref) => {
       }
     });
 
-    memoizedScrollAnimation(position, target, onUpdate);
+    memoizedScrollAnimation(position, target, isMobileOrTablet, onUpdate);
   }, []);
 
   useEffect(() => {
@@ -125,9 +138,9 @@ export const WebgiViewer = forwardRef((props, ref) => {
     setPreviewMode(false);
 
     gsap.to(positionRef, {
-      x: 1.56,
-      y: 5.0,
-      z: 0.01,
+      x: !isMobile ? 1.56 : 9.36,
+      y: !isMobile ? 5.0 : 10.95,
+      z: !isMobile ? 0.01 : 0.09,
       scrollTrigger: {
         trigger: ".display-section",
         start: "top bottom",
@@ -142,9 +155,9 @@ export const WebgiViewer = forwardRef((props, ref) => {
     });
 
     gsap.to(target, {
-      x: -0.55,
-      y: 0.32,
-      z: 0.0,
+      x: !isMobile ? -0.55 : 1.62,
+      y: !isMobile ? 0.32 : 0.02,
+      z: !isMobile ? 0.0 : -0.06,
       scrollTrigger: {
         trigger: ".display-section",
         start: "top bottom",
